58653694e10ae893b71fe0d5e8f6a6a4a459ebe2
Ashish558 <77892788+Ashish558@users.noreply.github.com>
2022-03-07 12:52:48 +0530

    Update styles.js

--- client/src/pages/styles/styles.js
+++ client/src/pages/styles/styles.js
@@ -16,18 +16,18 @@ export const containerStyle = {
     overflowY: 'scroll',
     ['@media (max-width:970px)']: { // eslint-disable-line no-useless-computed-key
         py: '81.1px',
-        pb: '10px'
+        pb: '30px'
     },
     ['@media (max-width:568px)']: { // eslint-disable-line no-useless-computed-key
         px: '16px',
         py: '73.1px',
-        pb: '10px'
+        pb: '30px'
     },
     ['@media (min-width:970px)']: { // eslint-disable-line no-useless-computed-key
         py: '40px',
         ml: '300px',
         width: 'auto',
-        pb: '10px'
+        pb: '30px'
     }
 }
 
